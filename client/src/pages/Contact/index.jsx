5611e0003f5eb597921420164fa8c7f70b2034c8
404pandas <mary.panda.jackson@gmail.com>
2025-07-15 13:21:27 -0400

    Possible cors fix

--- client/src/pages/Contact/index.jsx
+++ client/src/pages/Contact/index.jsx
@@ -24,7 +24,7 @@ function Contact() {
     if (!errorMessage && topic && email && message) {
       try {
         const response = await fetch(
-          "https://react-portfolio-iqr5.onrender.com:5001/api/contact",
+          "https://react-portfolio-iqr5.onrender.com/api/contact",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
