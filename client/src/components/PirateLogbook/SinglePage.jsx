aa2beafabe861cc238ff19a6d4689e3d3fd9ea80
404pandas <mary.panda.jackson@gmail.com>
2026-03-21 17:23:30 -0500

    Figured out where the end was

--- client/src/components/PirateLogbook/SinglePage.jsx
+++ client/src/components/PirateLogbook/SinglePage.jsx
@@ -3,7 +3,7 @@ import PageContent from "./PageContent";
 export default function SinglePage({ page, logs }) {
   const data = logs[page];
 
-  if (!data) return <div className="page">The End</div>;
+  if (!data) return <div className="page">To be continued...</div>;
 
   return (
     <div className="single-page">
